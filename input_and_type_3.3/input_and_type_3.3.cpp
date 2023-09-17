5517658328ffe0a17e93ec3993d5fd48cc25b083
Joshua <jlmeeks587@gmail.com>
2023-09-17 19:05:02 -0400

    3.3 test

--- input_and_type_3.3/input_and_type_3.3.cpp
+++ input_and_type_3.3/input_and_type_3.3.cpp
@@ -2,9 +2,8 @@
 
 int main() {
 	cout << "Please enter your first name and age\n";
-	string first_name;
-	int age;
-	cin >> first_name;
-	cin >> age;
-	cout << "Hello, " << first_name << " (age " << age << ")\n"; 
+	string first_name = "???";
+	double age = 0;
+	cin >> first_name >> age;
+	cout << "Hello, " << first_name << " (age " << age * 12 << " months)\n"; 
 }
